33737b1cf3ee3571ff7143f1e51fac025738703b
Benito Cabral <benitocabralneres@gmail.com>
2020-05-09 11:30:31 -0300

    fixed isNaN without parameter

--- contador/scripts/main.js
+++ contador/scripts/main.js
@@ -1,13 +1,16 @@
 function addUP(id) {
     let totalid = document.getElementById("total");
-    let totalvalue = parseInt(totalid.nodeValue, 10);
-    totalvalue = isNaN ? 0 : totalvalue;
+    let totalvalue = parseInt(totalid.value, 10);
+    totalvalue = isNaN(totalvalue) ? 0 : totalvalue;
+    let itemid = document.getElementById(id);
+    let itemvalue = parseInt(itemid.value, 10);
+    itemvalue = isNaN(itemvalue) ? 0 : itemvalue;
     if (totalvalue < 100) {
-        let itemid = document.getElementById(id);
-        let itemvalue = parseInt(itemid.nodeValue,10);
-        itemvalue = isNaN ? 0 : itemvalue;
         totalvalue++;
+        totalid.value = totalvalue;
         itemvalue++;
+        itemid.value = itemvalue;
+
         itemid.innerHTML = itemvalue;
         totalid.innerHTML = totalvalue;
     }
